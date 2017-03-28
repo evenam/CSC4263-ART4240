5e32dfc8ed7b609610a29a22772619c3d0e3cb8b
DanielMano <mano.daniel@yahoo.com>
2017-03-28 13:37:39 -0500

    Song select, adv screen, and static filter
    Got song select w/ rotating poly working using placeholder graphics as
    well as difficulty selection. Have an advanced gameplay screen and a
    preliminary static filter on all screens.

---
+++ game/Assets/scripts/RotateSongSelect.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSongSelect : MonoBehaviour
+{
+    public bool canRotate;
+
+    void Start()
+    {
+        canRotate = true;
+    }
+
+    void Update ()
+    {
+        if (canRotate)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                canRotate = false;
+                rotateLeft();
+            }
+            else if (Input.GetKeyDown(KeyCode.J))
+            {
+                canRotate = false;
+                rotateRight();
+            }
+        }
+    }
+
+    public void rotateLeft()
+    {
+        if (canRotate)
+        {
+            canRotate = false;
+            StartCoroutine(RotateCoroutine(Vector3.up * 72, 1));
+        }
+    }
+
+    public void rotateRight()
+    {
+        if (canRotate)
+        {
+            canRotate = false;
+            StartCoroutine(RotateCoroutine(Vector3.up * -72, 1));
+        }
+    }
+
+    public IEnumerator RotateCoroutine(Vector3 angles, float time)
+    {
+        Quaternion fAngle = transform.rotation;
+        Quaternion tAngle = Quaternion.Euler(transform.eulerAngles + angles);
+        for(float t = 0F; t < 1F; t += Time.deltaTime/time)
+        {
+            transform.rotation = Quaternion.Lerp(fAngle, tAngle, t);
+            yield return null;
+        }
+        canRotate = true;
+    }
+}
