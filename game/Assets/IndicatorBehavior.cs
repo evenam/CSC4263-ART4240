1614a050bd62a48801485c56d75e327878ad81bb
Everett Moser <foolsboy137@gmail.com>
2017-04-04 18:18:46 -0500

    Indicator object

---
+++ game/Assets/IndicatorBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorBehavior : MonoBehaviour {
+
+	public float animationTime = 1.0f;
+	public float time = 0.0f;
+
+	void Start() {
+		gameObject.GetComponent<Animator> ().speed = 1.03f / animationTime;
+		Destroy (gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length); 
+	}
+
+	void Update() {
+		
+	}
+}
