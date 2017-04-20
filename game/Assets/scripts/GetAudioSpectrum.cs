0411774bd2671c7e19e84f0b4514de5ba1d8e563
DanielMano <mano.daniel@yahoo.com>
2017-04-19 21:24:47 -0500

    Prelim spectometer
    Still can't figure out how to move it, maybe someone else can figure it
    out.

---
+++ game/Assets/scripts/GetAudioSpectrum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAudioSpectrum : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public static float[] samples = new float[512];
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ game/Assets/scripts/SpectrumToVisual.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectrumToVisual : MonoBehaviour {
+
+    public GameObject sampleCubePrefab;
+    GameObject[] sampleCube = new GameObject[512];
+    public float maxScale;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject instanceSampleCube = (GameObject)Instantiate(sampleCubePrefab);
+            instanceSampleCube.transform.position = this.transform.position;
+            instanceSampleCube.transform.parent = this.transform;
+            instanceSampleCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            instanceSampleCube.transform.position = Vector3.forward * 10;
+            sampleCube[i] = instanceSampleCube;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if (sampleCube != null)
+            {
+                sampleCube[i].transform.localScale = new Vector3(1, (GetAudioSpectrum.samples[i] * maxScale) + 2, 10);
+            }
+        }
+    }
+}
