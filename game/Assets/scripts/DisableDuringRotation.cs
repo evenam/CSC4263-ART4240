95191fb5333f30b435785beb99c9219cbfd5331b
Everett Moser <foolsboy137@gmail.com>
2017-04-23 15:29:39 -0500

    Revert "Revert "Fixed some things with song select""
    This reverts commit f60e54cbb8540a6f3d6ce3a89c3b11df74d39bbc.

---
+++ game/Assets/scripts/DisableDuringRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableDuringRotation : MonoBehaviour {
+
+    public Button buttonToBeDisabled;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void DisableButton()
+    {
+        buttonToBeDisabled.interactable = false;
+        StartCoroutine(WaitForRotate());
+    }
+
+    public IEnumerator WaitForRotate()
+    {
+        yield return new WaitForSeconds(1);
+        buttonToBeDisabled.interactable = true;
+    }
+}
